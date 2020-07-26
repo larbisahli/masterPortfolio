52868bc13e33f929084910446717da57bbd9c3e9
Larbi Sahli <larbisahli1905@gmail.com>
2020-07-26 13:49:48 +0100

    c

--- src/components/githubRepoCard/GithubRepoCard.js
+++ src/components/githubRepoCard/GithubRepoCard.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ProjectLanguages from '../../components/projectLanguages/ProjectLanguages';
 import "./GithubRepoCard.css";
-import {Fade} from "react-reveal";
+import { Fade } from "react-reveal";
 
 export default function GithubRepoCard({ repo, theme }) {
-  function openRepoinNewTab(url) {
-    var win = window.open(url, "_blank");
-    win.focus();
-  }
+	function openRepoinNewTab(url) {
+		var win = window.open(url, "_blank");
+		win.focus();
+	}
 
-  return (
+	return (
 		<div>
 			<Fade bottom duration={2000} distance="40px">
 				<div
