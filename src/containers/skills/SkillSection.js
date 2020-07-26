f5ba1eff0c19e5c5a8f3484efbe6278e05ce1c73
Larbi Sahli <larbisahli1905@gmail.com>
2020-07-26 13:49:48 +0100

    c

--- src/containers/skills/SkillSection.js
+++ src/containers/skills/SkillSection.js
@@ -8,13 +8,13 @@ import FullStackImg from './FullStackImg';
 import CloudInfraImg from "./CloudInfraImg";
 import DesignImg from "./DesignImg";
 
-function GetSkillSvg(props){
-  if(props.fileName==="DataScienceImg")
-      return <DataScienceImg theme={props.theme} />;
-  else if(props.fileName==="FullStackImg")
-      return <FullStackImg theme={props.theme} />;
-  else if(props.fileName==="CloudInfraImg")
-      return <CloudInfraImg theme={props.theme} />;
+function GetSkillSvg(props) {
+  if (props.fileName === "DataScienceImg")
+    return <DataScienceImg theme={props.theme} />;
+  else if (props.fileName === "FullStackImg")
+    return <FullStackImg theme={props.theme} />;
+  else if (props.fileName === "CloudInfraImg")
+    return <CloudInfraImg theme={props.theme} />;
   return <DesignImg theme={props.theme} />;
 }
 
@@ -38,7 +38,7 @@ class SkillSection extends Component {
 
               <div className="skills-text-div">
                 <Fade right duration={1000}>
-                  <h1 className="skills-heading" style={{color: theme.text }}>{skill.title}</h1>
+                  <h1 className="skills-heading" style={{ color: theme.text }}>{skill.title}</h1>
                 </Fade>
                 <Fade right duration={1500}>
                   <SoftwareSkill logos={skill.softwareSkills} />
@@ -47,13 +47,13 @@ class SkillSection extends Component {
                   <div>
                     {skill.skills.map(skillSentence => {
                       return (
-												<p
-													className="subTitle skills-text"
-													style={{ color: theme.secondaryText }}
-												>
-													{skillSentence}
-												</p>
-											);
+                        <p
+                          className="subTitle skills-text"
+                          style={{ color: theme.secondaryText }}
+                        >
+                          {skillSentence}
+                        </p>
+                      );
                     })}
                   </div>
                 </Fade>
